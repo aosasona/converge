788a2b2a27c96a7d5c97581f3771ab519fa2af9f
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2025-10-25 23:55:06 +0100

    Add procedure type enum

--- procedure/procedure.go
+++ procedure/procedure.go
@@ -1,6 +1,23 @@
 package procedure
 
+//go:generate go tool github.com/abice/go-enum --marshal
+
+// ENUM(query,mutation,live)
+type ProcedureType string
+
 type Handler[In, Out any] func(c *Context, input In) (Out, error)
 
 // TODO: define procedure interface
-type Procedure interface{}
+type Procedure[I, O any] interface {
+	// Name returns the name of the procedure
+	Name() string
+
+	// Type returns the type of the procedure (query, mutation, live)
+	// A query is a read-only operation
+	// A mutation is a write operation that may change state
+	// A live procedure is a long-lived operation that streams updates (usually backed by websockets; implementation might differ)
+	Type() ProcedureType
+
+	// Handle processes the input and returns the output or an error
+	Handle(c *Context, input I) (O, error)
+}
